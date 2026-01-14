fad2f9fbdbc73c20f6f45c2f14055f709aed57ea
Samuel Jun <mrsamueljun@gmail.com>
2026-01-14 18:51:02 -0500

    Add comment about sync storage limit

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -4,6 +4,9 @@ import { DEFAULT_SETTINGS, STORAGE_KEY } from "./constants";
 
 export default class Settings {
   constructor() {
+    // Sync storage limits (approximately 100 KB, 8 KB per item)
+    // Firefox: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/storage/sync
+    // Chrome: https://developer.chrome.com/docs/extensions/reference/storage/#sync
     this.storage = browser.storage.sync || browser.storage.local;
   }
 
